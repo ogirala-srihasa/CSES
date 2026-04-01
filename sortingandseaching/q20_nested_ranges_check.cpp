bb8f3e2cb1f65f3b77b53bc15e2bed6290991c72
InfiniteOS6 <infiniteos456@gmail.com>
2026-04-01 16:02:29 +0530

    q43

--- sortingandseaching/q20_nested_ranges_check.cpp
+++ sortingandseaching/q20_nested_ranges_check.cpp
@@ -1,8 +1,27 @@
+// Given n ranges, your task is to determine for each range if it contains some
+// other range and if some other range contains it. Range [a,b] contains range
+// [c,d] if a \le c and d \le b.
+
 #include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-int main(){
+  int n;
+  cin >> n;
 
-    
+  vector<vector<int>> arr(n, vector<int>(2));
+  vector<vector<int>> arr_copy(n, vector<int>(2));
+  vector<int> ans1(n);
+  vector<int> ans2(n);
+
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i][0] >> arr[i][1];
+    arr_copy[i][0] = arr[i][0];
+    arr_copy[i][1] = arr[i][1];
+  }
+
+  sort(arr_copy.begin(), arr_copy.end());
 }
