4e37bbaeeb88f903ba6dd63764946e5b98e66ca8
InfiniteOS6 <infiniteos456@gmail.com>
2026-04-14 22:01:34 +0530

    nested ranges check

--- sortingandseaching/q20_nested_ranges_check.cpp
+++ sortingandseaching/q20_nested_ranges_check.cpp
@@ -5,23 +5,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Range{
+  public:
+  int start;
+  int end;
+  int index;
+
+  Range(int start, int end, int index){
+    this -> start = start;
+    this -> end = end;
+    this -> index = index;
+  }
+
+  Range(){
+    this -> start = 0;
+    this -> end = 0;
+    this -> index = 0;
+  }
+
+  bool operator<(const Range&other) const{
+    if(start < other.start) return true;
+    else if(start == other.start) return end > other.end;
+    return false;
+  }
+};
+
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   int n;
   cin >> n;
-
-  vector<vector<int>> arr(n, vector<int>(2));
-  vector<vector<int>> arr_copy(n, vector<int>(2));
+  vector<Range> arr(n);
+  for(int i = 0; i < n; i++){
+    arr[i].index = i;
+    cin >> arr[i].start >> arr[i].end;
+  }
   vector<int> ans1(n);
   vector<int> ans2(n);
+  sort(arr.begin(), arr.end());
 
-  for (int i = 0; i < n; i++) {
-    cin >> arr[i][0] >> arr[i][1];
-    arr_copy[i][0] = arr[i][0];
-    arr_copy[i][1] = arr[i][1];
+  int max_end = INT_MIN;
+  int min_end = INT_MAX;
+  for(int i = 0; i < n; i++){
+    if(arr[i].end <= max_end){
+      ans1[arr[i].index] = 1;
+    }else max_end = arr[i].end;
+    if(arr[n-i-1].end >= min_end) ans2[arr[n-i-1].index] = 1;
+    else min_end = arr[n-i-1].end;
   }
 
-  sort(arr_copy.begin(), arr_copy.end());
+  for(int i = 0; i < n; i++){
+    cout << ans2[i] << ' ';
+  }
+  cout << '\n';
+  for(int i = 0; i < n; i++){
+    cout << ans1[i] << ' ';
+  }
+
 }
